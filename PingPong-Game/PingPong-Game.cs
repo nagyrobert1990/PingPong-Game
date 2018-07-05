1cf5e971e3d9e35d189c916a6a26e5db6696728b
nagyrobert1990 <nagy.robert1990@gmail.com>
2018-07-05 13:48:58 +0200

    sticks are controllable with keys

--- PingPong-Game/PingPong-Game.cs
+++ PingPong-Game/PingPong-Game.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,10 +20,61 @@ namespace PingPong_Game
 
         private void PingPong_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.Escape)
+            switch (e.KeyCode)
             {
-                this.Close();
+                case Keys.Escape:
+                    this.Close();
+                    break;
+                case Keys.S:
+                    leftMoveDown();
+                    break;
+                case Keys.W:
+                    leftMoveUp();
+                    break;
+                case Keys.Down:
+                    rightMoveDown();
+                    break;
+                case Keys.Up:
+                    rightMoveUp();
+                    break;
             }
+        }
+
+        private void leftMoveUp()
+        {
+            if (this.pictureBox1.Location.Y > 0)
+            {
+                this.pictureBox1.Location = new Point(this.pictureBox1.Location.X, (this.pictureBox1.Location.Y - 20));
+            }
+        }
+
+        private void leftMoveDown()
+        {
+            if (this.pictureBox1.Bottom <= this.ClientSize.Height)
+            {
+                this.pictureBox1.Location = new Point(this.pictureBox1.Location.X, (this.pictureBox1.Location.Y + 20));
+            }
+        }
+
+        private void rightMoveUp()
+        {
+            if (this.pictureBox2.Location.Y > 0)
+            {
+                this.pictureBox2.Location = new Point(this.pictureBox2.Location.X, (this.pictureBox2.Location.Y - 20));
+            }
+        }
+
+        private void rightMoveDown()
+        {
+            if (this.pictureBox2.Bottom <= this.ClientSize.Height)
+            {
+                this.pictureBox2.Location = new Point(this.pictureBox2.Location.X, (this.pictureBox2.Location.Y + 20));
+            }
+        }
+
+        private void ballMoving()
+        {
+            
         }
     }
 }
